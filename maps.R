7b4111988c7de018be0ff7ce4715542be6a23473
Grant Nolasco <nolasco@aurora>
2018-12-05 08:45:43 -0800

    Added the country code and maps with US and no US

--- maps.R
+++ maps.R
@@ -5,21 +5,93 @@
 # === Libraries === 
 library(tmap)
 library(rworldmap)
+library(tidyverse)
+library(sf)
+library(countrycode)
+library(spdplyr)
 
 # === Datasets === 
 ## uncomment if not loaded into environment
-#twitter_merged <- read.csv("twitter_merged.csv", stringsAsFactors = FALSE)
-#twitter_merged_noRT <- read.csv("twitter_merged_noRT.csv", stringsAsFactors = FALSE)
+twitter_merged <- read.csv("twitter_merged.csv", stringsAsFactors = FALSE)
+twitter_merged_noRT <- read.csv("twitter_merged_noRT.csv", stringsAsFactors = FALSE)
 
-count_location <- twitter_merged %>%
-                        group_by(country) %>%
-                            summarise(count = n())
+## uncomment if not correct working directory
+#setwd('/home/nolasco/soc-twitter')
+#list.files()
 
-count_location$count <- as.numeric(count_location)
-count_location$count[2]
+# Creating a Country Code column for the datasets 
+twitter_merged['countryCode'] <- countrycode(twitter_merged$country, 'country.name', 'iso3c')
+twitter_merged_noRT['countryCode'] <- countrycode(twitter_merged_noRT$country, 'country.name', 'iso3c')
 
-tm_shape(count_location) +
-  tm_polygons(count)
 
-World$HPI
-count_location$count
+# Not all countries had a proper match, hence I had to add it manually
+no_matchCountry <- c('Belgique', 'Deutschland', 'España', 'Kenia', 'Nederland', 
+                     'Österreich', 'Panamá', 'Roumanie', 'Spanje', '台灣', 'भारत')
+no_matchCode <- c('BEL', 'DEU', 'ESP', 'KEN', 'NLD', 
+                  'AUT', 'PAN', 'ROU', 'ESP', 'TWN', 'IND')
+
+for (i in 1:length(no_matchCountry)){
+  twitter_merged$countryCode[twitter_merged$country == no_matchCountry[i]] <- no_matchCode[i]
+  twitter_merged_noRT$countryCode[twitter_merged_noRT$country == no_matchCountry[i]] <- no_matchCode[i]
+}
+
+
+# Creating a count dataframe
+count.RT <- twitter_merged %>%
+                          count(countryCode) %>%
+                              na.omit()
+
+count.noRT <- twitter_merged_noRT %>%
+                          count(countryCode) %>%
+                              na.omit()
+
+
+# Changing column name so it isn't similar to function name
+colnames(count.RT)[2] <- 'Frequency'
+colnames(count.noRT)[2] <- 'Frequency'
+
+
+# Converting the count dataframe into sp class
+count.RTsp <- joinCountryData2Map(count.RT, joinCode = "ISO3", nameJoinColumn = "countryCode")
+count.noRTsp <- joinCountryData2Map(count.noRT, joinCode = "ISO3", nameJoinColumn = "countryCode")
+
+## Dataframes not including US (might be more efficient code involving spdplyr)
+# Creating a count dataframe
+count.RT_US <- twitter_merged %>%
+  count(countryCode) %>%
+  na.omit() %>%
+  filter(countryCode != 'USA')
+
+count.noRT_US <- twitter_merged_noRT %>%
+  count(countryCode) %>%
+  na.omit() %>%
+  filter(countryCode != 'USA')
+
+
+# Changing column name so it isn't similar to function name
+colnames(count.RT_US )[2] <- 'Frequency'
+colnames(count.noRT_US )[2] <- 'Frequency'
+
+
+# Converting the count dataframe into sp class
+count.RT_USsp <- joinCountryData2Map(count.RT_US, joinCode = "ISO3", nameJoinColumn = "countryCode")
+count.noRT_USsp <- joinCountryData2Map(count.noRT_US, joinCode = "ISO3", nameJoinColumn = "countryCode")
+
+# === Interactive Maps ===
+tm_shape(count.RTsp) +
+  tm_polygons('Frequency') + tm_layout(main.title = "With RT")
+
+tm_shape(count.noRTsp) + 
+  tm_polygons('Frequency') + tm_layout(main.title = "No RT")
+
+
+## As we can tell, US has the most number of tweets regarding soil
+# Since US is such a huge outlier, we probably should remove it to see 
+tm_shape(count.RT_USsp) +
+  tm_polygons('Frequency') + tm_layout(main.title = "With RT (not including US)")
+
+tm_shape(count.noRT_USsp) +
+  tm_polygons('Frequency') + tm_layout(main.title = "No RT (not including US)")
+
+
+## Ask if we should do state level
