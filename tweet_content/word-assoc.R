892145155bbe37c7bd06467a6332b569a48a9e52
KyleMonper <kyle.monper@gmail.com>
2019-04-23 10:30:48 -0700

    remove extraneous files

--- tweet_content/word-assoc.R
+++
@@ -1,6 +0,0 @@
-## Notes on the word association
-# Plans:
-# - Find all the unique query terms and associate them into the four groups
-# - If the query term appears, then replace it with the title of the group (one word)
-#    - The reason for this is because it is hard to do phrase association/our query words are one or two words
-# - Perform word association on the given data (more details later)
