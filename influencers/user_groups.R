5d2266ec5d9fb6091d09af03dd596b0d33b31350
KyleMonper <kyle.monper@gmail.com>
2019-04-26 12:51:15 -0700

    began categorizing users

---
+++ influencers/user_groups.R
@@ -0,0 +1,207 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#  most influential tweets for different user groups   #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Questions to answer:
+
+#What tweets are most liked/retweeted by farmers? by non-profit staff? by academics?
+##~~ approach: 
+#~~~~ Id the users who retweeted the most RTed tweets
+#~~~~ perform user search and try to separate them into "groups"
+  
+#Are there differences in the content/message of these tweets that make them more appealing to these different groups?
+#~~~~ once users have been 'grouped' figure out which groups favored (RTed) which tweets the most.
+  
+
+library(tidyverse)
+library(stringr)
+library(rtweet)
+library(wordcloud)
+library(tidytext)
+
+source("text_analysis_functions.R")
+
+twitter_merged <- read.csv("twitter_merged.csv", stringsAsFactors = FALSE) %>% 
+  distinct()
+twitter_merged_noRT <- read.csv("twitter_merged_noRT.csv", stringsAsFactors = FALSE) %>% 
+  distinct()
+
+
+
+
+##### creating a column to flag tweets from or about India ####
+RT <- twitter_merged %>% 
+  mutate(
+    is_india = case_when(
+      str_detect(tolower(text), "[\u0900-\u097F]+|india|crore|health card|rupee|narendramodi|managed to feed 1.25 billion people|akshaykumar") ~ 1,
+      str_detect(tolower(screen_name), "[\u0900-\u097F]+|india|crore|health card|rupee|narendramodi") ~ 1,
+      str_detect(tolower(hashtags), "[\u0900-\u097F]+|india|crore|health card|rupee|narendramodi") ~ 1))
+
+#replace na w/ 0
+RT$is_india[is.na(RT$is_india)] <- 0
+
+
+RT <- RT %>% 
+  filter(!str_detect(tolower(text), "pontifex"), #remove the popes RTs
+         screen_name != "Pontifex") 
+
+
+
+
+
+
+
+
+##### STEP 1: identify user groups ####
+
+#ID 'active users' i.e those who have tweeted >32 times this equates to ~every other week over 16 months
+active_users <- RT %>% 
+  filter(is_india == 0) %>% 
+  group_by(screen_name) %>% 
+  summarise(
+    total_tweets = n(),
+    total_retweeted = sum(retweet_count, na.rm=T)
+  ) %>% 
+  filter(total_tweets >= 16)
+
+#get tweet info on active users
+user_list <- RT %>% 
+  filter(screen_name %in% active_users$screen_name)
+
+# see which users among TNC's partners are among the active user list
+partner_overlap <- user_list %>% 
+  filter(screen_name %in% partners$Handle | screen_name %in% partners$associatedHandles)
+unique(partner_overlap$screen_name)
+# [1] "NACDconserve"   "USDA_NRCS"      "soil_institute" "MidwestRowCrop"
+# [5] "GrowingReturns" "AGreeAgPolicy"  "nobleresinst"   "USDA"          
+# [9] "SoilPartners"   "FoundationFAR"  "ASA_CSSA_SSSA" 
+
+
+
+#look up user info from twitter API
+user_info <- lookup_users(unique(user_list$user_id))
+
+##### create word list for IDing user groups #### 
+
+
+#use partner list and associated user group `$Type` to create part of word list
+partner_type <- partners %>% 
+  select(Handle, associatedHandles, Type) %>% 
+  separate(., associatedHandles, c("a", "b", "c", "d"), sep = "[;]") %>% 
+  gather(x, handle, -Type) %>% 
+  select(handle, Type) %>% 
+  na.omit()
+ 
+
+
+#function for getting words from each parterns description based on thei group type
+usr_group_words <- function(x) {
+  type <- partner_type %>% 
+    filter(Type == x)
+  
+  usr <- lookup_users(type$handle)
+  
+  usr_desc <- usr %>% 
+  select(description)
+  
+  names(usr_desc) <- "text"
+  words <- prepare_text(usr_desc)
+  return(words)
+}
+
+## get word list for each group type
+research <- usr_group_words("Research")
+media <- usr_group_words("Media")
+lab <- usr_group_words("Laboratory")
+govt <- usr_group_words("Government")
+non_prof <- usr_group_words("Non-profit")
+foundation <- usr_group_words("Foundation")
+corperate <- usr_group_words("Corporate")
+
+
+#view in df
+search_terms <- data.frame(research[1:20,1], media[1:20,1], lab[1:20,1], govt[1:20,1], non_prof[1:20,1], foundation[1:20,1], corperate[1:20,1])
+names(search_terms) <- c("rs", "media", "lab", "govt", "nonProfit", "foundation", "corporate")
+
+
+
+
+##view descriptions for top active users (also get text from last tweet to help in IDing)
+usr_desc <- user_info %>% 
+  select(screen_name,description,text)
+
+# explore: make wordcloud of descriptions
+#rename descprtion column to text (this is required of the create_wordcloud function - may change this later)
+cloud <- user_info %>% 
+  select(description)
+names(cloud) <- "text"
+create_wordcloud(cloud)
+
+#### build word list
+science_WL <- c('ologist', 'science', 'scientist', 'university', 'data', 'studies', 'research', 'agronomy', 'institute')
+farmer_WL <- c("farm", "rancher", "ranching", "cattle", "sheep")
+govt_WL <- c("endorse", "nrcs", "usda", "gov", "public") 
+business_WL <- c("company" , "business" ,"customer", "entrepreneur")
+media_WL <- c("edit", "journalist")
+environmental_WL <- c("conserv", "sustain", "water", "organic", "climate", "environment")
+
+
+### Id users ###
+
+# choosing a top RT, US specific tweet:
+
+herdyshepherd_tweet <- twitter_merged_noRT %>% 
+  filter(screen_name == "herdyshepherd1") %>% #read through the list of top tweets and selected one that was not related to India
+  arrange(-retweet_count) %>% 
+  head(1)
+
+
+#use this text to look for all the instances of a retweet 
+herdys_RT_users <- twitter_merged %>% 
+  filter(
+    str_detect(text, "TBF eating in an ethically sound manner") #took a quote from the text, as calling paste(herdyshepherd_tweet$text) did not work
+  ) 
+
+#for some reason lookup_user wouldn't work on the _RT_users df so i took the users directly from twitter_merged
+herdy_user_list <- twitter_merged %>% 
+  filter(screen_name %in% herdys_RT_users$screen_name)
+
+#get user info
+herdy_user_info <- lookup_users(unique(herdy_user_list$user_id))
+
+
+#### categorize users 
+herdy_user_info <- herdy_user_info %>% 
+  select(screen_name, description) %>% 
+  mutate(
+    is_scientist = case_when(str_detect(tolower(description), paste(science_WL, collapse = "|" )) ~ 1),
+    is_farmer = case_when(str_detect(tolower(description), paste(farmer_WL, collapse = "|" )) ~ 1),
+    is_govt = case_when(str_detect(tolower(description), paste(govt_WL, collapse = "|" )) ~ 1),
+    is_business = case_when(str_detect(tolower(description), paste(business_WL, collapse = "|" )) ~ 1),
+    is_media = case_when(str_detect(tolower(description), paste(media_WL, collapse = "|" )) ~ 1),
+    is_envnmtal = case_when(str_detect(tolower(description), paste(environmental_WL, collapse = "|" )) ~ 1)
+  )
+
+herdy_user_info[is.na(herdy_user_info)] <- 0
+
+prop_likes <- herdy_user_info %>% 
+  group_by(.[,3:8])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
