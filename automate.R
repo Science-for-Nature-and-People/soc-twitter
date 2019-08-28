27708cc8780d3c26c10e0997fcbaafaf2e910ed1
jeremyknox-ucsb <knox@aurora.nceas.ucsb.edu>
2019-08-28 12:57:35 -0700

    Reorganized, fixed duplicate issue with RT/favorite counts, removed redundant code, confirmed all code runs properly

--- automate.R
+++ automate.R
@@ -1,6 +1,11 @@
 # Twitter
 # API only has data going back 6-9 days
 # devtools::install_github("mkearney/rtweet")
+
+
+
+
+## LIBRARIES ----
 library(tidyverse)
 library(rtweet)
 library(countrycode)
@@ -10,17 +15,27 @@ library(jsonlite)
 library(streamR)
 library(httr)
 
-## DO NOT run the write.csv lines when making edits to script (make a seperate path in your own home folder on aurora)  
+
+
+
+
+## DO NOT run the write.csv lines when making edits to script ----
+# (make a seperate path in your own home folder on aurora)  
 
 path <- '/home/shares/soilcarbon/Twitter' # LOCATION OF MASTER FILES
 
-## Read previous data ----
 
-# Master files 
+
+
+## READ PREVIOUS (MASTER) DATA ----
+
 twitter_merged.master <- read.csv(file.path(path, 'Merged_v2/twitter_merged_v2.csv'), stringsAsFactors = FALSE) 
 twitter_merged_noRT.master <- read.csv(file.path(path, 'Merged_v2/twitter_merged_noRT_v2.csv'), stringsAsFactors = FALSE) 
 
-## Query the Twitter API for the latest data ----
+
+
+
+## **QUERY** TWITTER API FOR LAST 6-9 DAYS OF TWEET DATA ----
 
 # Create token
 twitter_token <- readRDS('twitter_token.rds')
@@ -35,10 +50,15 @@ tag_list <- as.character(tag_file$tag_list)
 q <- unname(sapply(tag_list, function(x) toString(dQuote(x))))
 
 # Searching tweets with query above (THIS CODE SEARCHES TWITTER FOR TERMS LISTED IN tag_list OVER THE LAST 6-9 DAYS)
-twitterAPI_new.prac <- search_tweets2(q, n = 100000, token=twitter_token, retryonratelimit = T)
+twitterAPI_new <- search_tweets2(q, n = 100000, token=twitter_token, retryonratelimit = T)
+
+
+
+
+## CLEAN NEW DATA ----
 
 # Make it a data frame
-twitterAPI_new <- as.data.frame(twitterAPI_new.prac, stringsAsFactors = FALSE)
+twitterAPI_new <- as.data.frame(twitterAPI_new, stringsAsFactors = FALSE)
 
 # Collapsing hashtags column 
 new_hashtags <- c()
@@ -66,7 +86,12 @@ twitterAPI_new <- twitterAPI_new %>%
 # Remove row names
 rownames(twitterAPI_new) <- NULL
 
-######### JB (We should save the dataframe to csv at this point appending the date of the day to the name)
+
+
+
+## **EXPORT** CLEANED RAW DATA ----
+
+# saving data as .csv file
 # Creating file name 
 file.name <- paste0(path, '/API_csv/', Sys.Date(), '.csv')
 
@@ -76,14 +101,15 @@ twitterAPI_new[i] <- lapply(twitterAPI_new[i], as.character)
 
 # Creating csv file
 write.csv(twitterAPI_new, file.name)
-######### JB
 
-## Prepare the data for the merge ----
+
+
+
+## PREPARE DATA FOR MERGE ----
 
 # Changing the type of specific columns
 twitter_merged.master$created_at <- as_datetime(twitter_merged.master$created_at)
 twitter_merged_noRT.master$created_at <- as_datetime(twitter_merged_noRT.master$created_at)
-twitterAPI_new$created_at <- as_datetime(twitterAPI_new$created_at)
 
 # Creating provenance columns w/ value as API
 twitterAPI_new <- add_column(twitterAPI_new, provenance = "API", .before = 1)  
@@ -110,22 +136,6 @@ for (i in 1:length(twitterAPI_new$country_code)){
 UID <- c((dim(twitter_merged.master)[1]+1):(dim(twitter_merged.master)[1]+dim(twitterAPI_new)[1]))
 twitterAPI_new$UID <- UID
 
-# REMOVE DUPLICATES: in twitterAPI_new based on created_at and text columns
-twitterAPI_new <- twitterAPI_new %>% 
-  distinct(created_at, text, .keep_all = TRUE)
-
-######### JB 
-# REMOVE DUPLICATES: when comparing twitterAPI_new and twitter_merged.master, 
-# with the same values in the five columns chosen (I did this instead of doing duplicated function due to UID column)
-uniqueRows <- !(do.call(paste0, twitterAPI_new[,c("created_at", "user_id", "screen_name", "text", "source")]) %in% 
-                  do.call(paste0, twitter_merged.master[,c("created_at", "user_id", "screen_name", "text", "source")]))
-twitterAPI_new <- twitterAPI_new[uniqueRows,]
-######### JB
-
-# Creating new dataframe with no RTs (if error exists, restart R session and run code again)
-twitterAPI_new_noRT <- twitterAPI_new %>% 
-                          filter(is_retweet == FALSE)
-
 # Mutate 'Hits' column with keyword hits from tweet text
 # Function to replace `character(0)` with NAs as NULL values are dropped when flattening list
 charnull_set <- function(x){
@@ -137,7 +147,7 @@ charnull_set <- function(x){
 # Text bits to search through # keywords = query words
 keywords <- paste0(tag_list, collapse = "|")
 
-## Store the matches as a new columns with words seprated by `;`
+# Store the matches as a new columns with words seprated by `;`
 twitterAPI_new <- twitterAPI_new %>%
   mutate(hits = str_extract_all(text, pattern = regex(keywords, ignore_case=TRUE)) %>%  # Extract all the keywords
            map(~charnull_set(.x)) %>%   # Replace character(0) with NAs
@@ -145,14 +155,29 @@ twitterAPI_new <- twitterAPI_new %>%
            tolower) %>% # all our keywords are lower case
               distinct()
 
-twitterAPI_new_noRT <- twitterAPI_new_noRT %>%
-  mutate(hits = str_extract_all(text, pattern = regex(keywords, ignore_case=TRUE)) %>%  # Extract all the keywords
-           map(~charnull_set(.x)) %>%   # Replace character(0) with NAs
-           map_chr(~glue::glue_collapse(.x, sep = ";")) %>%   # collapse the multiple hits/collapse instead of glue_collapse
-           tolower) %>% # all our keywords are lower case %>%
-              distinct()
-              
-## Merging and exporting data ----
+
+
+
+## REMOVE OLD TWEETS THAT ARE DUPLICATES OF NEWLY SCRAPED TWEETS ----
+
+# looking through new tweets and comparing to old tweets in master data frame then
+# removing tweets from old df as to keep the most up-to-date tweets
+uniqueRows <- !(do.call(paste0, twitter_merged.master[,c("created_at", "user_id", "screen_name", "text", "source")]) %in% 
+                  do.call(paste0, twitterAPI_new[,c("created_at", "user_id", "screen_name", "text", "source")]))
+twitter_merged.master <- twitter_merged.master[uniqueRows,]
+
+
+
+
+
+## SEPARATE INTO RT AND NORT DATA FRAMES ----
+twitterAPI_new_noRT <- twitterAPI_new %>% 
+  filter(is_retweet == FALSE)
+
+
+
+
+## MERGING AND **EXPORTING** DATA ----
 
 # Merging datasets together using rbind 
 twitter_merged_new <- rbind(twitter_merged.master, twitterAPI_new)
