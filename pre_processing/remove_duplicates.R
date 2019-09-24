dce0aa65b0bb950c78ea4a47c27988894150208b
jeremyknox-ucsb <knox@aurora.nceas.ucsb.edu>
2019-09-24 14:49:42 -0700

    update for v3 master DFs

--- pre_processing/remove_duplicates.R
+++ pre_processing/remove_duplicates.R
@@ -12,8 +12,8 @@ path <- '/home/shares/soilcarbon/Twitter/'
 
 
 # read in data frames
-RT_dups <- read.csv(paste(path, '/Merged_v2/twitter_merged_v2.csv', sep = ""), stringsAsFactors = FALSE)
-noRT_dups <- read.csv(paste(path, '/Merged_v2/twitter_merged_noRT_v2.csv', sep = ""), stringsAsFactors = FALSE)
+RT_dups <- read.csv(paste(path, '/Merged_v3/twitter_merged_v3.csv', sep = ""), stringsAsFactors = FALSE)
+noRT_dups <- read.csv(paste(path, '/Merged_v3/twitter_merged_noRT_v3.csv', sep = ""), stringsAsFactors = FALSE)
 
 
 # remove duplicates based on 'created_at' and 'text' columns
@@ -24,6 +24,6 @@ noRT <- noRT_dups %>%
 
 
 # write out data frames 
-write.csv(RT, paste(path, '/Merged_v2/', 'twitter_merged_v2.csv', sep = ""), row.names = FALSE)
-write.csv(noRT, paste(path, '/Merged_v2/', 'twitter_merged_noRT_v2.csv', sep = ""), row.names = FALSE)
+write.csv(RT, paste(path, '/Merged_v3/', 'twitter_merged_v3.csv', sep = ""), row.names = FALSE)
+write.csv(noRT, paste(path, '/Merged_v3/', 'twitter_merged_noRT_v3.csv', sep = ""), row.names = FALSE)
 
