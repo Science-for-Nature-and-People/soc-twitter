cdd4598f3ca8ec3166f34bd02c35d0aa9ab4d79f
KyleMonper <kyle.monper@gmail.com>
2019-11-22 16:12:56 -0800

    building networks

--- tweet_content/content_creators/gen_data.R
+++ tweet_content/content_creators/gen_data.R
@@ -1,35 +1,16 @@
 #### generate data for network analysis on content creators
 
 library(rtweet)
-library(tidygraph)
-library(ggraph)
-library(here)
+library(tidyverse)
+source("../../text_analysis_functions.R")
 
-### use `content_creator` variable from the top_users.Rmd scripts
-
-## get user_id's from screen names
-tmp <- left_join(content_creator, noRT_no_india, by = "screen_name")
-users <- unique(tmp$user_id)
+noRT <- read.csv("/home/shares/soilcarbon/Twitter/Merged_v3/twitter_merged_noRT_v3.csv", stringsAsFactors = FALSE)
 
 
 
-
-##### this was outside of aurora (on local R machine) to avoid issues associated w/ the rcurl package
-# for (i in 1:length(users)){
-#   if (i == 1){
-#     friends <- get_friends(users[i])
-#   } else {
-#     tmp <- get_friends(users[i])
-#     friends <- rbind(friends, tmp)
-#   }
-#   
-#   
-#   # pause if divisible by 15
-#   if (i %% 15 == 0){
-#     write_csv(friends, "user_friends.csv")
-#     print(Sys.time())
-#     Sys.sleep(15*61)
-#   }
-# }
-
+top_RT <- noRT_clean %>% 
+  group_by(screen_name) %>% 
+  dplyr::summarise(
+    total_RT = sum(retweet_count)
+  )
 
