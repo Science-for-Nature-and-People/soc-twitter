3062c1f447dd96ddc817a2166fdf4ac9039f9119
Julien Brun <brun@nceas.ucsb.edu>
2019-12-10 15:04:32 -0800

    updating the plot of presence/absence of tweets per day

--- pre_processing/potentially_useful_scripts/missing_date_viz.R
+++ pre_processing/potentially_useful_scripts/missing_date_viz.R
@@ -21,36 +21,41 @@ flag_india <- function(data) {
 
 path <- '/home/shares/soilcarbon/Twitter' # LOCATION OF MASTER FILES
 
-twitter_merged <- read.csv(file.path(path, 'Merged_v2/twitter_merged_v2.csv'), stringsAsFactors = FALSE) 
-#twitter_merged_noRT <- read.csv(file.path(path, 'Merged_v2/twitter_merged_noRT_v2.csv'), stringsAsFactors = FALSE) 
+twitter_merged <- read.csv(file.path(path, 'Merged_v3/twitter_merged_v3.csv'), stringsAsFactors = FALSE) 
+#twitter_merged_noRT <- read.csv(file.path(path, 'Merged_v3/twitter_merged_noRT_v3.csv'), stringsAsFactors = FALSE) 
 
-# DF of days when there are tweets 
+# DF of unique days when there are tweets 
 RT_unique <- twitter_merged %>% 
   mutate(created_at = ymd_hms(created_at)) %>% 
-  mutate(created_at = round_date(created_at, unit = "day")) %>% 
-  distinct(created_at, .keep_all = TRUE) %>% 
-  select(created_at) %>% 
-  mutate(created_at = ymd(created_at))
+  mutate(created_at_day = round_date(created_at, unit = "day")) %>%
+  distinct(created_at_day, .keep_all = TRUE) %>%
+  select(created_at_day) %>%
+  mutate(created_at_day = ymd(created_at_day))
+
 # plot of missing days in master data frame
-max_date <- max(RT_unique$created_at)
-RT_unique_viz <- ggplot(RT_unique, aes(x = created_at)) +
+max_date <- max(RT_unique$created_at_day)
+RT_unique_viz <- ggplot(RT_unique, aes(x = created_at_day)) +
   geom_bar(stat = 'count') +
-  scale_x_date(date_breaks = "1 month", date_labels = "%y %b") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
   geom_segment(aes(x=ymd("2017-04-01"),xend=ymd("2017-04-01"),y=0,yend=1.05)) +
-  annotate("text", x = ymd("2017-04-01"), y = 1.1, label = "Purchased ->") +
+  annotate("text", x = ymd("2017-04-01"), y = 1.1, label = "Purchased ->", size = 3) +
   geom_segment(aes(x=ymd("2017-10-11"),xend=ymd("2017-10-11"),y=0,yend=1.05)) +
-  annotate("text", x = ymd("2017-10-11"), y = 1.1, label = "Manual API ->") +
+  annotate("text", x = ymd("2017-10-11"), y = 1.1, label = "Manual API ->", size = 3) +
   geom_segment(aes(x=ymd("2019-06-15"),xend=ymd("2019-06-15"),y=0,yend=1.05)) +
-  annotate("text", x = ymd("2019-06-15"), y = 1.1, label = "Automatic API ->") +
+  annotate("text", x = ymd("2019-06-15"), y = 1.1, label = "Automatic API ->", size = 3) +
   geom_segment(aes(x=max_date,xend=max_date,y=0,yend=1.05)) +
-  annotate("text", x = max_date, y = 1.1, label = "End of Data") +
+  annotate("text", x = max_date, y = 1.1, label = "End of Data", size = 3) +
   theme_classic() +
   theme(legend.position = "none",
         panel.grid = element_blank(),
         axis.title = element_blank(),
-        axis.text.y = element_blank())
+        axis.text.y = element_blank(),
+        axis.text.x  = element_text(angle=90, vjust=0.5),
+        axis.ticks.y = element_blank()) 
+        # text = element_text(size=10))
 
-
+RT_unique_viz
+ggsave(RT_unique_viz, file = "./Figures/days_with_tweets.png", dpi = 300, width=10, height=4)
 
 
 
@@ -60,28 +65,32 @@ RT_count <- twitter_merged %>%
   mutate(created_at = round_date(created_at, unit = "day")) %>% 
   select(created_at) %>% 
   mutate(created_at = ymd(created_at))
+
 # plot of distibution (histogram) of number of tweets per day 
 int <- interval(min(RT_count$created_at), max(RT_count$created_at))
 RT_count_viz <- ggplot(RT_count, aes(x = created_at)) +
   geom_histogram(bins = time_length(int, "day")) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%y %b") +
-  theme_classic()
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
+  ylim(0,2000) + xlab(NULL) +
+  theme_classic() + theme(axis.text.x  = element_text(angle=90, vjust=0.5), text = element_text(size=14))
+  
+RT_count_viz
 
-# DF of count of tweets per day 
-RT_count_rm <- twitter_merged %>% 
-  mutate(created_at = ymd_hms(created_at)) %>% 
-  mutate(created_at = round_date(created_at, unit = "day")) %>%   
-  mutate(created_at = ymd(created_at)) %>% 
-  filter(created_at != ymd("2018-01-10") &
-           created_at != ymd("2017-08-22") &
-           created_at != ymd("2019-07-06") &
-           created_at != ymd("2019-07-07")) %>% 
-  select(created_at)
-# plot of distibution (histogram) of number of tweets per day, outliers removed (n > 2,500)
-RT_count_viz_rm <- ggplot(RT_count_rm, aes(x = created_at)) +
-  geom_histogram(bins = time_length(int, "day")) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%y %b") +
-  theme_classic()
+# # DF of count of tweets per day 
+# RT_count_rm <- twitter_merged %>% 
+#   mutate(created_at = ymd_hms(created_at)) %>% 
+#   mutate(created_at = round_date(created_at, unit = "day")) %>%   
+#   mutate(created_at = ymd(created_at)) %>% 
+#   filter(created_at != ymd("2018-01-10") &
+#            created_at != ymd("2017-08-22") &
+#            created_at != ymd("2019-07-06") &
+#            created_at != ymd("2019-07-07")) %>% 
+#   select(created_at)
+# # plot of distibution (histogram) of number of tweets per day, outliers removed (n > 2,500)
+# RT_count_viz_rm <- ggplot(RT_count_rm, aes(x = created_at)) +
+#   geom_histogram(bins = time_length(int, "day")) +
+#   scale_x_date(date_breaks = "1 month", date_labels = "%y %b") +
+#   theme_classic()
 
 
 # extra code
