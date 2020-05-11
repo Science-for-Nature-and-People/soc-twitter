74f30852899e2bb18e1e0e11952ca42b31e5028a
KyleMonper <kyle.monper@gmail.com>
2020-05-11 11:39:36 -0700

    fixed gap issue

--- pre_processing/potentially_useful_scripts/missing_date_viz.R
+++ pre_processing/potentially_useful_scripts/missing_date_viz.R
@@ -33,12 +33,21 @@ RT_unique <- twitter_merged %>%
   mutate(created_at = round_date(created_at, unit = "day")) %>% 
   distinct(created_at, .keep_all = TRUE) %>% 
   dplyr::select(created_at) %>% 
-  mutate(created_at = ymd(created_at))
+  mutate(created_at = ymd(created_at),
+         present = 1)
 
 # plot of missing days in master data frame
 max_date <- max(RT_unique$created_at)
-RT_unique_viz <- ggplot(RT_unique, aes(x = created_at)) +
-  geom_bar(stat = 'count',position ="identity") +
+min_date <- min(RT_unique$created_at)
+
+all_days <- data.frame(created_at = seq.Date(min_date, max_date, "day"))
+
+combined <- left_join(all_days, RT_unique) 
+yes_no <- combined %>% 
+  mutate(present = if_else(is.na(present), 0, 1))
+
+RT_unique_viz <- ggplot(RT_unique, aes(created_at)) +
+  geom_histogram(bins = 1134) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b'%y") +
   geom_segment(aes(x=ymd("2017-04-01"),xend=ymd("2017-04-01"),y=0,yend=1.05)) +
   ggplot2::annotate("text", x = ymd("2017-04-01"), y = 1.1, label = "Purchased ->", size = 5) +
@@ -47,12 +56,20 @@ RT_unique_viz <- ggplot(RT_unique, aes(x = created_at)) +
   geom_segment(aes(x=ymd("2019-06-15"),xend=ymd("2019-06-15"),y=0,yend=1.05)) +
   ggplot2::annotate("text", x = ymd("2019-06-15"), y = 1.1, label = "Automatic API ->", size = 5) +
   geom_segment(aes(x=max_date,xend=max_date,y=0,yend=1.05)) +
-  ggplot2::annotate("text", x = max_date, y = 1.1, label = "End of Data", size = 5) 
+  ggplot2::annotate("text", x = max_date, y = 1.1, label = "End of Data", size = 5) +
+  theme_classic() +
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.text.x = element_text(size = 16, angle = 90, vjust=0.5),
+        axis.ticks.y = element_blank(), 
+        text = element_text(size=14))
 
-  mutate(created_at_day = round_date(created_at, unit = "day")) %>%
-  distinct(created_at_day, .keep_all = TRUE) %>%
-  select(created_at_day) %>%
-  mutate(created_at_day = ymd(created_at_day))
+
+
+
+
 
 # plot of missing days in master data frame
 max_date <- max(RT_unique$created_at_day)
@@ -67,13 +84,12 @@ RT_unique_viz <- ggplot(RT_unique, aes(x = created_at_day)) +
   annotate("text", x = ymd("2019-06-15"), y = 1.1, label = "Automatic API ->", size = 3) +
   geom_segment(aes(x=max_date,xend=max_date,y=0,yend=1.05)) +
   annotate("text", x = max_date, y = 1.1, label = "End of Data", size = 3) +
-
   theme_classic() +
   theme(legend.position = "none",
         panel.grid = element_blank(),
         axis.title = element_blank(),
         axis.text.y = element_blank(),
-        axis.text.x = element_text(size = 16, angle = 90))
+        axis.text.x = element_text(size = 16, angle = 90),
         axis.text.x  = element_text(angle=90, vjust=0.5),
         axis.ticks.y = element_blank(), 
         text = element_text(size=14))
