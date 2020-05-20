107436f8f2a2740539998e072cc67bb58ebad04d
KyleMonper <kyle.monper@gmail.com>
2020-05-20 11:09:29 -0700

    fixed path

--- paper_figures/clean_data.R
+++ paper_figures/clean_data.R
@@ -1,6 +1,8 @@
 ##### cleaning master data set for use in other scripts (specifically for manuscript figures)
 ## takes v4 master, cleans data, remove duplicates, filter before may-2020
 ## only need to run once
+library(tidyverse)
+source("text_analysis_functions.R")
 
 ### most recent merged data
 noRT <- read.csv("/home/shares/soilcarbon/Twitter/Merged_v4/twitter_merged_noRT_v4.csv", stringsAsFactors = FALSE)
@@ -53,8 +55,7 @@ write_csv(noRT_noindia, "/home/shares/soilcarbon/Twitter/cleaned_data/noRT_clean
 write_csv(RT_noindia, "/home/shares/soilcarbon/Twitter/cleaned_data/RT_clean.csv")
 
 write_csv(noRT_india, "/home/shares/soilcarbon/Twitter/cleaned_data/noRT_clean_india.csv")
-write_csv(RT_india, "/home/shares/soilcarbon/Twitter/cleaned_data/noRT_clean_india.csv")
-
+write_csv(RT_india, "/home/shares/soilcarbon/Twitter/cleaned_data/RT_clean_india.csv")
 
 
 
